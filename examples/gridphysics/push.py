5f498d28e299623b3559aa0c4f39e2a42f5482b0
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-05-27 18:11:44 +0100

    probably should version control this

---
+++ examples/gridphysics/push.py
@@ -0,0 +1,47 @@
+# run this using 'python -m examples.gridphysics.push' from py-vgdl root
+# https://github.com/schaul/py-vgdl
+# https://github.com/EssexUniversityMCTS/gvgai/wiki/VGDL-Language
+
+'''
+Push: A simple board game
+
+@author: Alexander Zacherl
+'''
+
+box_level = """
+wwwwwwwww
+w       w
+w   C   w
+w   x   w
+w  xxx  w
+w   x   w
+w       w
+wwwwwwwww
+"""
+
+push_game = """
+BasicGame frame_rate=30
+    SpriteSet
+        cursor > CursorAvatar stype=marker cooldown=10 singleton=True
+        marker > Marker stype=cursor
+        piece > Passive
+        pieceSelected > SelectedPiece cooldown=10 singleton=True color=BLUE stype=piece
+        test > Passive color=GREEN
+    LevelMapping
+        x > piece
+        C > cursor
+    InteractionSet
+        cursor wall > stepBack
+        piece piece > bounceForward
+        piece wall > killSprite
+        marker piece > killSprite
+        piece marker > transformTo stype=pieceSelected
+        piece pieceSelected > bounceForward
+        pieceSelected wall > stepBack
+    TerminationSet
+        SpriteCounter stype=piece limit=0 win=False
+"""
+
+if __name__ == "__main__":
+    from vgdl.core import VGDLParser
+    VGDLParser.playGame(push_game, box_level)
